3b1f28024481ef6736da1b4a71cf2021103ed047
ozamora- <ozamora-@student.42madrid.com>
2025-06-13 14:58:00 +0200

    Oops bug fxed

--- src/map_check.c
+++ src/map_check.c
@@ -6,7 +6,7 @@
 /*   By: ozamora- <ozamora-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 14:34:22 by ozamora-          #+#    #+#             */
-/*   Updated: 2025/06/13 14:55:16 by ozamora-         ###   ########.fr       */
+/*   Updated: 2025/06/13 14:57:43 by ozamora-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	check_file(t_game *game, const char *scene_dir)
 	fd = open(scene_dir, O_RDONLY);
 	if (fd == -1)
 		(free_game(game), my_mlx_err(SCENE_UNEXISTENT));
-	bytes_read = read(fd, buffeunable to open orr, 1);
+	bytes_read = read(fd, buffer, 1);
 	if (bytes_read == 0)
 		(free_game(game), close(fd), my_mlx_err(SCENE_EMPTY));
 	else if (bytes_read == -1)
